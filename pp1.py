3621457dc68fc812195cdf5b83687484a34fe370
Sanjana Devadiga <163403696+Sdevadiga2@users.noreply.github.com>
2026-02-13 21:52:31 +0530

    Program to calculate age difference and display love message
    This program takes boy and girl details and prints their age difference.

---
+++ pp1.py
@@ -0,0 +1,6 @@
+boy_name=input("boy name:")
+boy_age=int(input("boy age:"))
+girl_name=input("girl name:")
+girl_age=int(input("girl age:"))
+age_diff=boy_age-girl_age
+print(boy_name+" loves "+girl_name+" .and age difference between them is " +str(age_diff))
